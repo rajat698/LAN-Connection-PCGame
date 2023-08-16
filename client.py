28a8556cc7c7b72db0ad9105e859be28f98fdd96
rajat698 <67538489+rajat698@users.noreply.github.com>
2023-08-16 13:41:14 -0400

    UI created

--- client.py
+++ client.py
@@ -1,22 +1,78 @@
+import tkinter as tk
+import threading
+import subprocess
+import time
 import socket
 
 HEADER = 64
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-
+# DISCONNECT_MESSAGE = "!DISCONNECT"
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER, PORT))
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+PORT = 5050
 
-send("Hello World")
-send(DISCONNECT_MESSAGE)
+def server_address():
+    SERVER = host_entry.get()
+    return SERVER
+
+
+def check_connection():
+    host = host_entry.get()
+    result_label.config(text="Checking")
+
+    SERVER = server_address()
+    client.connect((SERVER, PORT))
+    
+    threading.Thread(target=ping_host, args=(host,)).start()
+    threading.Thread(target=print_messages).start()
+
+def print_messages():
+    
+    while True:
+        game_label.config(text=client.recv(2048).decode(FORMAT))
+
+def ping_host(host):
+
+    
+    while True:
+        try:
+            response = subprocess.run(["ping", "-c", "1", host], capture_output=True, text=True, timeout=5)
+            if response:
+                result_label.config(text="Host is connected")
+                # print(response)
+            else:
+                result_label.config(text="Host is not connected")
+        except subprocess.TimeoutExpired:
+            result_label.config(text="Host is not reachable")
+
+        time.sleep(5) #Sleep for 5 seconds
+
+# Create the main window
+root = tk.Tk()
+root.title("Client UI")
+
+window_width = 400
+window_height = 400
+root.geometry(f"{window_width}x{window_height}")
+
+# Create and pack widgets
+host_label = tk.Label(root, text="Enter host IP")
+host_label.pack()
+
+host_entry = tk.Entry(root)
+host_entry.pack()
+
+check_button = tk.Button(root, text="Connect", command=check_connection)
+check_button.pack()
+
+game_label = tk.Label(root, text="")
+game_label.pack()
+
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+listen_thread = threading.Thread(target=print_messages)
+listen_thread.start()
+
+# Start the main event loop
+root.mainloop()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import tkinter as tk
 
 HEADER = 64
 DISCONNECT_MESSAGE = "!DISCONNECT"
@@ -10,31 +11,126 @@ SERVER = socket.gethostbyname(socket.gethostname())
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((SERVER, PORT))
 
+clients = {}
+gamers = {}
+client_sockets = []
+offline_players = "3"
+
+Allowed_IPs = [socket.gethostbyname(socket.gethostname())]
+
+def add_IPs():
+    allowed = Allowed_IP_entry.get()
+    Allowed_IPs.append(allowed)
+    print(Allowed_IPs)
+
+def send_message_to_all_clients(message):
+    for client in client_sockets:
+        try:
+            client.send(message.encode())
+        except:
+            client_sockets.remove(client)
+
+
 def handle_client(connection, address):
-    print(f"[NEW CONNECTION] {address} connected")
 
-    connected = True
-    while connected:
-        msg_length = connection.recv(HEADER).decode(FORMAT)
-        if msg_length:
+    global offline_players
+    if address in clients:
+        print(f"[ALREADY CONNECTED] {address} tried to connect again.")
+        connection.close()
+        return
 
-            msg_length = int(msg_length)
-            msg = connection.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            
-            print(f"[{address}]{msg}")
-            connection.send("Msg received".encode(FORMAT))
+    if address not in gamers:
+        gamers[address] = "Gamer " + str(len(gamers) + 1)
     
+    if "Gamer 1" in gamers.values():
+        gamer1_label.config(text="Gamer 1: Online")
+    
+    if "Gamer 2" in gamers.values():
+        gamer2_label.config(text = "Gamer 2: Online")
+    
+    print(gamers)
+
+    clients[address] = connection
+    
+    print(f"[NEW CONNECTION] {address} connected")
+    
+    client_sockets.append(connection)
+
+    while True:
+        try:
+            data = connection.recv(1024).decode()
+            if not data:
+                break
+        except:
+            client_sockets.remove(connection)
+            break
+
     connection.close()
+    del clients[address]
+    
+
+    for key, value in gamers.items():
+        if key == address:
+            offline_players = value[len(value) - 1]
+
+    if offline_players == "1":
+        gamer1_label.config(text = "Gamer 1: Offline")
+
+    elif offline_players == "2":
+        gamer2_label.config(text = "Gamer 2: Offline")
+
+    print(f"[DISCONNECTED] {address} disconnected")
+
+    
+def send_Start():
+    send_message_to_all_clients("Game has started")
+
+def send_Pause():
+    send_message_to_all_clients("Game is paused")
 
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         connection, address = server.accept()
-        thread = threading.Thread(target = handle_client, args = (connection, address))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-              
-start()
+        client_address = address[0]
+
+        if client_address in Allowed_IPs:
+            thread = threading.Thread(target = handle_client, args = (connection, address))
+            thread.start()
+        print(f"[ACTIVE CONNECTIONS] {len(clients)}")
+        
+
+root = tk.Tk()
+root.title("Server UI")
+
+window_width = 400
+window_height = 400
+root.geometry(f"{window_width}x{window_height}")
+
+#Add allowed IP addresses
+Allowed_IP_label = tk.Label(root, text="Enter Gamer PC Allowed IPs")
+Allowed_IP_label.pack()
+
+Allowed_IP_entry = tk.Entry(root)
+Allowed_IP_entry.pack()
+
+Allowed_IP_button = tk.Button(root, text="Add", command=add_IPs)
+Allowed_IP_button.pack()
+
+gamer1_label = tk.Label(root, text="Games 1: Offline")
+gamer1_label.pack()
+
+gamer2_label = tk.Label(root, text="Gamer 2: Offline")
+gamer2_label.pack()
+
+start_button = tk.Button(root, text="Start Game", command=send_Start)
+start_button.pack()
+
+pause_button = tk.Button(root, text="Pause Game", command=send_Pause)
+pause_button.pack()
+
+listen_thread = threading.Thread(target=start)
+listen_thread.start()
+
+root.mainloop()
